c8222c54f53d2198bf43df9d6972a308e99d558c
Slava Agafonov <Agafonov.Slava@hotmail.com>
2021-05-17 00:30:56 -0700

    Remove scripts

--- Scripts/crawler/leetcode.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-browser = webdriver.PhantomJS()
-url = 'https://leetcode.com/problems/house-robber/description/'
-browser.get(url)
-soup = BeautifulSoup(browser.page_source, 'lxml')
-desc = soup.find("meta",  attrs={'name':'description'})
-print(desc["content"] if desc else "No meta desc given")
-browser.quit()
